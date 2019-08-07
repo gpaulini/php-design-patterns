25ab20e01da0a39dfe7f1d5e73a99f732ffd62d2
Gabriel Paulini <paulini.gp@gmail.com>
2019-08-07 11:03:48 -0300

    classe LogFile (sem implementação Singleton) criada

--- ativSup01.php
+++ ativSup01.php
@@ -7,25 +7,28 @@
 
 class LogFile
 {
-	public function registrarNovoEvento($nome, $mensagem)
-	{
-		$arquivo = $this->criarNovoArquivoLog($nome);
+	const FILE = '.log';
 
-		if (fwrite($arquivo, $mensagem)) return true;
+	public function registrarNovoEvento($evento)
+	{
+		$arquivo = $this->capturarArquivoLog();
+
+		return (fwrite($arquivo, $evento . "\n")) ? true : false;
 	}
 
-	private function criarNovoArquivoLog($nome)
+	private function capturarArquivoLog()
 	{
-		if (!is_dir('./logs')) mkdir('./logs');
-
-		$nome .= '_'.date('Y-m-d'); //xxxx_2019-08-01
-		
-		return fopen('./logs/'.$nome.'.txt', 'w');
+		return fopen(self::FILE, 'a');
 	}
 }
 
 $logger = new LogFile;
 
-if ($logger->registrarNovoEvento($_POST['nome'], $_POST['mensagem'])) {
-	header('Location: ./ativSup01.html');
+$descricaoEvento = date('Y-m-d H:i:s') . " >> {$_POST['botao']} foi clicado;";
+
+if ($logger->registrarNovoEvento($descricaoEvento)) {
+	echo json_encode(array('message' => '"' . $descricaoEvento . '" registrado', 'success' => true));
+	exit;
 }
+
+echo json_encode(array('message' => 'Falha no registro :(', 'success' => false));
